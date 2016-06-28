c8dd6245c2f8a7df0934db7210f86054b9789198
Trevor Blackwell <tlb@tlb.org>
2016-06-27 18:17:29 -0700

    log actions & rewards too

--- agent/logobs.py
+++ agent/logobs.py
@@ -3,13 +3,13 @@ import gym
 import numpy as np
 import cPickle as pickle
 
-# The world's simplest agent!
 class LoggingAgent(object):
     def __init__(self, action_space):
         self.action_space = action_space
 
     def act(self, observation, reward, done):
-        return np.zeros(self.action_space.shape)
+        return self.action_space.sample()
+        #return np.zeros(self.action_space.shape)
 
 if __name__ == '__main__':
     logger = logging.getLogger()
@@ -17,36 +17,34 @@ if __name__ == '__main__':
 
     env = gym.make('FetchRobotRGB-v0' if len(sys.argv)<2 else sys.argv[1])
 
-    # You provide the directory to write to (can be an existing
-    # directory, including one with existing data -- all monitor files
-    # will be namespaced). You can also dump to a tempdir if you'd
-    # like: tempfile.mkdtemp().
     outdir = '/tmp/logobs'
     os.mkdir(outdir)
 
-    # This declaration must go *after* the monitor call, since the
-    # monitor's seeding creates a new action_space instance with the
-    # appropriate pseudorandom number generator.
     agent = LoggingAgent(env.action_space)
 
     episode_count = 1
-    max_steps = 5
+    max_steps = 100
     reward = 0
     done = False
 
     for i in range(episode_count):
         ob = env.reset()
 
-        allobs = []
+        allobs = [ob]
+        allactions = []
+        allrewards = []
         for j in range(max_steps):
             action = agent.act(ob, reward, done)
-            obs, reward, done, _ = env.step(action)
-            allobs.append(obs)
+            ob, reward, done, _ = env.step(action)
+            allobs.append(ob)
+            allactions.append(action)
+            allrewards.append(reward)
             if done:
                 break
         obsf = open(os.path.join(outdir, 'ep%d.pickle' % i), 'wb')
         pickle.dump({
             'observations': allobs,
+            'actions': allactions
             'observation_space': env.observation_space,
             }, obsf)
         obsf.close()
